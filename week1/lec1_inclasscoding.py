84eafb66b055345e31f3df9e8b3ddae39429e8d5
zhanghao <zhangghaoo@gmail.com>
2017-05-19 08:41:18 +0800

    add classifier

--- week1/lec1_inclasscoding.py
+++ week1/lec1_inclasscoding.py
@@ -5,7 +5,10 @@ import tensorflow
 from matplotlib import pyplot as plt
 import time
 
-start_time = time.time()
+
+def distance(v1, v2):
+    return sum([(x - y) ** 2 for (x, y) in zip(v1, v2)]) ** (0.5)
+
 
 def draw_samples(n):
     A1 = np.array([[0.70710678118, 0.70710678118],
@@ -39,11 +42,45 @@ def plot_samples(samples):
         plt.scatter(sample[0][0], sample[0][1], color=color)
     plt.show()
 
-samples = draw_samples(1000)
 
-print(time.time()-start_time)
+def nnc(x_hat, Dn):
+    select = 0
+    nearest_distance = float("inf")
+    for i in range(len(Dn)):
+        d = distance(x_hat, Dn[i][0])
+        if d < nearest_distance:
+            nearest_distance = d
+            select = i
+    return Dn[select][1]
 
-plot_samples(samples)
+
+def g(x):
+    return -(np.dot(np.array([1, 1]),x) - 1.25)
 
 
+def f(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
 
+
+Dn = draw_samples(500)
+# plot_samples(samples)
+Tk = draw_samples(1000)
+
+print("Nearest Neighbor Classifier")
+correct = 0
+for sample in Tk:
+    predicted = nnc(sample[0], Dn)
+    if predicted == sample[1]:
+        correct += 1
+print("Error: " + str(round(1 - correct / 1000., 2)))
+
+print("Second Classifier")
+correct = 0
+for sample in Tk:
+    predicted = f(g(sample[0]))
+    if predicted == sample[1]:
+        correct += 1
+print("Error: " + str(round(1 - correct / 1000., 2)))
