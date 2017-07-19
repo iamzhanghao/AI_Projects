992caaadb9f20a064aa7e12f32c8df5455e566e2
Zhang Hao iMac <zhangghaoo@gmail.com>
2017-07-18 22:08:49 -0700

    GUI

--- Brc_Project/GUI_v2.py
+++ Brc_Project/GUI_v2.py
@@ -1,11 +1,12 @@
-from tkinter import Tk, Label, Button, filedialog
-import PIL
+from tkinter import Tk, Label, Button, filedialog, messagebox
+
+from PIL import ImageTk, Image
 
 
 class GUI:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
+        master.title("Cancer Classifier")
 
         self.label = Label(master, text="This is our first GUI!")
         self.label.pack()
@@ -13,13 +14,32 @@ class GUI:
         self.greet_button = Button(master, text="Select File", command=self.select_file)
         self.greet_button.pack()
 
+        self.classify_button = Button(master,text="Classify", command = self.classify)
+        self.classify_button.pack()
+
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
+
+        self.img = None
+        self.img_label = None
+
     def select_file(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                              filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
-        print(filename)
+                                              filetypes=(("png files", "*.png"), ("all files", "*.*")))
+
+        if filename !="":
+            self.img = ImageTk.PhotoImage(Image.open(filename))
+
+            # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+            self.img_label = Label(self.master, image=self.img)
+
+            # The Pack geometry manager packs widgets in rows or columns.
+            self.img_label.pack()
+            print(filename)
+
+    def classify(self):
+        messagebox.showinfo("Results", "I don'tknow")
 
 
 root = Tk()
