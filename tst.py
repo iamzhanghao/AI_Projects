c9c4acebc851d2a26008b6c692ad3d0dcf97d143
zhanghao <zhanghao>
2017-07-30 23:08:20 +0800

    GA

--- tst.py
+++ tst.py
@@ -2,9 +2,14 @@ import numpy as np
 
 from PIL import Image
 
-img = Image.open("C:\\Users\Hao\Desktop\SOB_B_A-14-22549AB-40-009.png")
+from random import *
 
-dataset = np.load("C:\\Users\Hao\Projects\AI_Projects\project\saved_dataset\dataset2.npy")[()]
+# img = Image.open("C:\\Users\Hao\Desktop\SOB_B_A-14-22549AB-40-009.png")
+#
+# dataset = np.load("C:\\Users\Hao\Projects\AI_Projects\project\saved_dataset\dataset2.npy")[()]
+#
+# for i in range(100):
+#     print(dataset['train_data'][i][1][1])
 
-for i in range(100):
-    print(dataset['train_data'][i][1][1])
+while True:
+    print(randint(0,7))
